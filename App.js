69c29fefbb27ffe82d631313a4c8213d305fe826
Aditya <47221162+adityasinghbaghel@users.noreply.github.com>
2021-08-12 10:06:05 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,78 @@
+import { StatusBar } from 'expo-status-bar';
+import React , {useEffect , useState} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
+import WeatherInfo from './components/Weatherinfo'
+
+
+const WEATHER_API_KEY = '8ed64fb8979dedcdeb3e780c2ba26318';
+const BASE_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?'
+
+export default function App() {
+const [errorMessage , setErrorMessage] = useState(null)
+const [currentWeather , setCurrentWeather] = useState(null)
+const [unitsSystem , setUnitsSystem]  = useState('metric')  
+useEffect(() => {
+    load()
+  } , [])
+
+  async function load(){
+    try {
+      let {status} = await Location.requestPermissionsAsync()
+    
+      if(status!= 'granted' ){
+        setErrorMessage('Access to location is needed to run the app')
+       return
+      }
+      const location = await Location.getCurrentPositionAsync()
+      
+
+
+      const {latitude , longitude} = location.coords
+      const weatherUrl = `${BASE_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=${unitsSystem}&appid=${WEATHER_API_KEY}`
+      const response = await fetch(weatherUrl)
+      const result = await response.json()
+      
+      if(response.ok){
+        setCurrentWeather(result)
+      } else{
+        setErrorMessage(result.message)
+      }
+
+    } catch (error) {
+      setErrorMessage(error.message)
+    }
+  }
+
+
+  if(currentWeather){
+    const {main : {temp},} = currentWeather
+    return (
+      <View style={styles.container}>
+      <StatusBar style="auto" />
+      <View style = {styles.main}>
+       <WeatherInfo currentWeather = {currentWeather} />
+      </View>
+      </View>
+     );
+    }else{
+       return(
+         <View style = {styles.container}>
+           <Text>{errorMessage}</Text>
+           <StatusBar style = "auto" />
+         </View>
+       )
+     }
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  main: {
+    justifyContent: 'center',
+    flex:1,
+  }
+});
